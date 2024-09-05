4bf95fab0442b78417bf6754e31c3b4344bdd740
Manace_Dc <72782699+manace07@users.noreply.github.com>
2024-09-05 15:40:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base : "/https://github.com/manace07/assessment/",
+  base : "/assessment/",
   server : {
     open: true,
     port : 3000,
